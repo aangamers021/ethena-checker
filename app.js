93bb49d970ee4b3a511b6e0c84820f3357394b5b
Ahmad Afham <54086061+aangamers021@users.noreply.github.com>
2024-04-02 16:11:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ document.getElementById('checkButton').addEventListener('click', () => {
         fetch(`https://claim.ethena.fi/api/claim?address=${address}`, {
             method: "GET",
             headers: {
-                'accept': '*/*',
-                'accept-language': 'en-US,en;q=0.9',
                 'referer': 'https://claim.ethena.fi/',
-                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
             },
         })
         .then(response => response.json())
